87a19fdaaaacdca8cc7e45c11dc9d2b9c8766e1e
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-04-11 18:41:44 -0400

    add syntax highlighting to man pages using the bat command

--- zsh/configs/colors.zsh
+++ zsh/configs/colors.zsh
@@ -6,3 +6,6 @@ colors
 
 # enable colored output from ls, etc. on FreeBSD-based systems
 export CLICOLOR=1
+
+# Syntax highlighting for man pages using bat
+export MANPAGER="sh -c 'col -bx | bat -l man -p'"
