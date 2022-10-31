cf61b9014c12218378f3a3cdcef322bd2c9806b6
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2022-10-31 17:58:03 -0400

    update lvim

--- lvim/config.lua
+++ lvim/config.lua
@@ -4,13 +4,15 @@ lvim.colorscheme = "onedark"
 lvim.leader = "space"
 
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["gs"] = false
+lvim.keys.normal_mode["<S-Right>"] = ":bnext"
+lvim.keys.normal_mode["<S-Left>"] = ":bprevious"
 
-lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.alpha.active = true
-
-lvim.builtin.notify.active = true
+lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 lvim.builtin.dap.active = true
 
 -- if you don't want all the parsers change this to a table of the ones you want
@@ -28,16 +30,24 @@ lvim.builtin.treesitter.ensure_installed = {
   "ruby",
   "java",
   "yaml",
+  "markdown"
 }
+
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 lvim.transparent_window = true
+
 -- Additional Plugins
 lvim.plugins = {
   { "navarasu/onedark.nvim" },
   { "GeekyAnts/native-base-vscode-snippets" },
   { "ntpeters/vim-better-whitespace" },
+  { "rcarriga/nvim-dap-ui" },
+  { "theHamsta/nvim-dap-virtual-text" },
+  { "Pocco81/DAPInstall.nvim" },
+  { "nvim-telescope/telescope-dap.nvim" },
+  { "christoomey/vim-tmux-navigator" },
   { "christoomey/vim-sort-motion",
     keys = { { "n", "gs" }, { "v", "gs" } } },
   { "tpope/vim-surround",
@@ -78,4 +88,108 @@ require("onedark").setup {
   transparent = true
 }
 
-lvim.keys.normal_mode["gs"] = false
+-- local dap = require('dap')
+
+-- dap.adapters.node2 = {
+--   type = 'executable',
+--   command = 'node',
+--   args = { os.getenv('HOME') .. '/dev/microsoft/vscode-node-debug2/out/src/nodeDebug.js' },
+-- }
+
+-- dap.configurations.javascript = {
+--   {
+--     name = 'Launch',
+--     type = 'node2',
+--     request = 'launch',
+--     program = '${file}',
+--     cwd = vim.fn.getcwd(),
+--     sourceMaps = true,
+--     protocol = 'inspector',
+--     console = 'integratedTerminal',
+--   },
+--   {
+--     -- For this to work you need to make sure the node process is started with the `--inspect` flag.
+--     name = 'Attach to process',
+--     type = 'node2',
+--     request = 'attach',
+--     processId = require 'dap.utils'.pick_process,
+--   },
+-- }
+
+-- dap.configurations.typescript = {
+--   {
+--     name = 'Launch',
+--     type = 'node2',
+--     request = 'launch',
+--     program = '${file}',
+--     cwd = vim.fn.getcwd(),
+--     sourceMaps = true,
+--     protocol = 'inspector',
+--     console = 'integratedTerminal',
+--   },
+--   {
+--     -- For this to work you need to make sure the node process is started with the `--inspect` flag.
+--     name = 'Attach to process',
+--     type = 'node2',
+--     request = 'attach',
+--     processId = require 'dap.utils'.pick_process,
+--   },
+-- }
+
+-- dap.adapters.chrome = {
+--   type = "executable",
+--   command = "node",
+--   args = { os.getenv("HOME") .. "~/dev/microsoft/vscode-chrome-debug/out/src/chromeDebug.js" }
+-- }
+
+-- dap.configurations.javascriptreact = { -- change this to javascript if needed
+--   {
+--     name = "Chrome",
+--     type = "chrome",
+--     request = "attach",
+--     program = "${file}",
+--     cwd = vim.fn.getcwd(),
+--     sourceMaps = true,
+--     protocol = "inspector",
+--     port = 9222,
+--     webRoot = "${workspaceFolder}"
+--   },
+--   {
+--     name = "React Native",
+--     type = "node2",
+--     request = "attach",
+--     program = "${file}",
+--     cwd = vim.fn.getcwd(),
+--     sourceMaps = true,
+--     protocol = "inspector",
+--     console = "integratedTerminal",
+--     port = 35000,
+--   }
+-- }
+
+-- dap.configurations.typescriptreact = { -- change to typescript if needed
+--   {
+--     name = "Chrome",
+--     type = "chrome",
+--     request = "attach",
+--     program = "${file}",
+--     cwd = vim.fn.getcwd(),
+--     sourceMaps = true,
+--     protocol = "inspector",
+--     port = 9222,
+--     webRoot = "${workspaceFolder}"
+--   },
+--   {
+--     name = "React Native",
+--     type = "node2",
+--     request = "attach",
+--     program = "${file}",
+--     cwd = vim.fn.getcwd(),
+--     sourceMaps = true,
+--     protocol = "inspector",
+--     console = "integratedTerminal",
+--     port = 35000,
+--   }
+-- }
+
+-- require('dap.ext.vscode').load_launchjs(nil, {})
