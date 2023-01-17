e6bb1e61b71784dcf449f80feb901118a202d5e3
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2023-01-17 13:23:50 -0500

    add beautysh formatter to format shell scripts

--- lvim/lua/user/formatters.lua
+++ lvim/lua/user/formatters.lua
@@ -18,5 +18,21 @@ formatters.setup {
   {
     command = "sql-formatter",
     filetypes = { "sql" }
+  },
+  {
+    command = "beautysh",
   }
 }
+
+--indent-size INDENT_SIZE, -i INDENT_SIZE
+-- Sets the number of spaces to be used in indentation.
+--backup, -b          Beautysh will create a backup file in the same path as
+-- the original.
+--check, -c           Beautysh will just check the files without doing any
+-- in-place beautify.
+--tab, -t             Sets indentation to tabs instead of spaces.
+--force-function-style FORCE_FUNCTION_STYLE, -s FORCE_FUNCTION_STYLE
+-- Force a specific Bash function formatting. See below
+-- for more info.
+--version, -v         Prints the version and exits.
+--help, -h            Print this help message.
