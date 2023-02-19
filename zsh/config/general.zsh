99aec374305faf47a465645d0d0eb18b1a33a3ae
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2023-02-19 10:24:44 -0500

    :recycle: Refactor Zsh config

--- zsh/config/general.zsh
+++ zsh/config/general.zsh
@@ -3,6 +3,9 @@
 export EDITOR="lvim"
 export HOMEBREW_CASK_OPTS="--no-quarantine"
 
+eval "$(direnv hook $SHELL)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 PATH=$PATH:"/Users/steve/.local/bin"
 
 sz() { source ~/.zshrc }
@@ -15,32 +18,32 @@ sum() { paste -sd+ - | bc }
 igrep() { grep -i $@ }
 
 restart-postgres() {
-  rm /usr/local/var/postgres/postmaster.pid && ( \
-    cd ~/Library/LaunchAgents && \
-      launchctl unload homebrew.mxcl.postgresql.plist && \
-      launchctl load -w homebrew.mxcl.postgresql.plist \
-  )
+    rm /usr/local/var/postgres/postmaster.pid && ( \
+            cd ~/Library/LaunchAgents && \
+            launchctl unload homebrew.mxcl.postgresql.plist && \
+            launchctl load -w homebrew.mxcl.postgresql.plist \
+        )
 }
 
 start_dev_server() {
-  if [ -f yarn.lock ]
-  then
-    yarn start
-  elif [ -f package.json ]
-  then
-    npm start
-  elif [ -f index.html ]
-  then
-    browser-sync start --server --files '**'
-  else
-    echo "No package.json or index.html file found"
-  fi
+    if [ -f yarn.lock ]
+    then
+        yarn start
+    elif [ -f package.json ]
+    then
+        npm start
+    elif [ -f index.html ]
+    then
+        browser-sync start --server --files '**'
+    else
+        echo "No package.json or index.html file found"
+    fi
 }
 
 backup_home_brew() {
-  CURRENT=$(pwd)
-  cd ~/.dotfiles
-  brew bundle dump -f --describe --no-upgrade
-  cd $CURRENT
-  echo "Brew Bundle Successfully Created"
+    CURRENT=$(pwd)
+    cd ~/.dotfiles
+    brew bundle dump -f --describe --no-upgrade
+    cd $CURRENT
+    echo "Brew Bundle Successfully Created"
 }
